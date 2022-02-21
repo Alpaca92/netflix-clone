e43da0a8275bccd3f3a51a527e6ccaa604f7d59c
alpaca <zxcvbnm5288@gmail.com>
2022-02-21 15:19:50 +0900

    add prev and next button

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,7 +1,11 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import styled from "styled-components";
 import { ApiData } from "../api";
+
+interface Action {
+  type: "INCREMENT" | "DECREMENT" | "RESIZE";
+}
 
 const Container = styled.div`
   padding: 0 20px;
@@ -38,45 +42,78 @@ function Carousel({ data }: { data?: ApiData }) {
     switch (true) {
       case width <= 500:
         return 2;
-      case width <= 800:
+      case 500 < width && width <= 800:
         return 3;
-      case width <= 1100:
+      case 800 < width && width <= 1100:
         return 4;
-      case width <= 1400:
+      case 1100 < width && width <= 1400:
         return 5;
       default:
         return 6;
     }
   };
+
   const [offset, setOffset] = useState(
     calculateRelativeOffset(window.innerWidth)
   );
-  const [page, setPage] = useState(0);
+  const [lastPage, setLastPage] = useState(
+    Math.floor((data?.results.length || 0) / offset) - 1
+  );
+
+  const reducer = (state: number, action: Action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state === lastPage ? 0 : state + 1;
+      case "DECREMENT":
+        return state === 0 ? lastPage : state - 1;
+      case "RESIZE":
+        return state > lastPage ? lastPage : state;
+      default:
+        return state;
+    }
+  };
+
+  const [page, dispatch] = useReducer(reducer, 0);
+
+  const nextPage = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const prevPage = () => {
+    dispatch({ type: "DECREMENT" });
+  };
 
   useEffect(() => {
-    window.addEventListener("resize", () =>
-      setOffset(calculateRelativeOffset(window.innerWidth))
-    );
+    const onResize = () => {
+      setOffset(calculateRelativeOffset(window.innerWidth));
+    };
 
-    return () =>
-      window.removeEventListener("resize", () =>
-        setOffset(calculateRelativeOffset(window.innerWidth))
-      );
+    window.addEventListener("resize", onResize);
+
+    return () => window.removeEventListener("resize", onResize);
   }, []);
+
+  useEffect(() => {
+    setLastPage(Math.floor((data?.results.length || 0) / offset) - 1);
+  }, [offset]);
+
+  useEffect(() => {
+    dispatch({ type: "RESIZE" });
+  }, [lastPage]);
 
   return (
     <Container>
       <AnimatePresence>
         <Row>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9]
+          {data?.results
             .slice(offset * page, offset * page + offset)
             .map((movie) => (
-              <Box>{movie}</Box>
+              <Box key={movie.id}>{movie.title}</Box>
             ))}
         </Row>
       </AnimatePresence>
-      <button>{"<"}</button>
-      <button>{">"}</button>
+      <button onClick={prevPage}>{"<"}</button>
+      <button onClick={nextPage}>{">"}</button>
     </Container>
   );
 }
